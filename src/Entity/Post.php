23609b23d6dd36fd90d4f9461feba7218cb6ee3d
Artjom Kuznetsov <artjom.kuznetsov1733@gmail.com>
2023-12-03 16:03:32 +0200

    add entity 'post'

---
+++ src/Entity/Post.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+class Post
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $companyName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $companyEmail = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $companyAddress = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $contactPhoneNumber = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $requirements = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $body = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $posted_at = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $posted_until = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCompanyName(): ?string
+    {
+        return $this->companyName;
+    }
+
+    public function setCompanyName(string $companyName): static
+    {
+        $this->companyName = $companyName;
+
+        return $this;
+    }
+
+    public function getCompanyEmail(): ?string
+    {
+        return $this->companyEmail;
+    }
+
+    public function setCompanyEmail(string $companyEmail): static
+    {
+        $this->companyEmail = $companyEmail;
+
+        return $this;
+    }
+
+    public function getCompanyAddress(): ?string
+    {
+        return $this->companyAddress;
+    }
+
+    public function setCompanyAddress(string $companyAddress): static
+    {
+        $this->companyAddress = $companyAddress;
+
+        return $this;
+    }
+
+    public function getContactPhoneNumber(): ?string
+    {
+        return $this->contactPhoneNumber;
+    }
+
+    public function setContactPhoneNumber(string $contactPhoneNumber): static
+    {
+        $this->contactPhoneNumber = $contactPhoneNumber;
+
+        return $this;
+    }
+
+    public function getRequirements(): ?string
+    {
+        return $this->requirements;
+    }
+
+    public function setRequirements(string $requirements): static
+    {
+        $this->requirements = $requirements;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): static
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    public function getPostedAt(): ?\DateTimeImmutable
+    {
+        return $this->posted_at;
+    }
+
+    public function setPostedAt(\DateTimeImmutable $posted_at): static
+    {
+        $this->posted_at = $posted_at;
+
+        return $this;
+    }
+
+    public function getPostedUntil(): ?\DateTimeImmutable
+    {
+        return $this->posted_until;
+    }
+
+    public function setPostedUntil(\DateTimeImmutable $posted_until): static
+    {
+        $this->posted_until = $posted_until;
+
+        return $this;
+    }
+}
